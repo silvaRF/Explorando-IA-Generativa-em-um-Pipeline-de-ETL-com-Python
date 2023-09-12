1947dfa77c02bbe5d7a5b2c89c0d9b46c67f5582
Rafael Fiuza <tifiuza@hotmail.com>
2023-09-12 16:58:41 -0300

    Acerto no cenário especial

--- SantanderDevWeek2023.py
+++ SantanderDevWeek2023.py
@@ -7,29 +7,33 @@ def create_news(first_name, account_balance, credit_card, limit_card, avg_amount
     list_news = []
     if(avg_amount_spent > (limit_card*60)/100):
         # dicas de controle de gastos
-        msg_um = {"id": 1,
-                  "description": search_message(first_name, 'Controle de Gastos')}
+        msg_um = {'id': 1,
+                  'description': search_message(first_name, 'Controle de Gastos')}
         list_news.append(msg_um)
+
     if(credit_card == 'SIM' and avg_amount_spent == 0):
         # informação de crédito disponível
-        msg_dois = {"id": 2,
-                    "description": search_message(first_name, 'Cartão de Crédito Disponível')}
+        msg_dois = {'id': 2,
+                    'description': search_message(first_name, 'Cartão de Crédito Disponível')}
         list_news.append(msg_dois)
         
     if(account_balance > 500):
         # informação sobre investimentos e opções disponíveis
-        msg_tres = {"id": 3,
-                    "description": search_message(first_name, 'Investimentos\Tipos de Investimento')}
+        msg_tres = {'id': 3,
+                    'description': search_message(first_name, 'Investimentos\Tipos de Investimento')}
         list_news.append(msg_tres)
     
+    if not list_news:
+        msg_quatro =  {'id': 4,
+                       'description': 'É um prazer tê-lo como nosso cliente. Conte sempre conosco.'}
+        list_news.append(msg_quatro)
 
-    return list_news if list_news else 'É um prazer tê-lo como nosso cliente. Conte sempre conosco.'
+    return list_news
 
 def search_message(first_name, type_message):
     arq_message = 'Levantamento_mensagens [PROJETO 001 - SDW_2023].xlsx'
     indice_random = 0
     message = ''
-    print(type_message)
     while message == '':
         df_message = pd.read_excel(arq_message)
         indice_random = random.randint(1, len(df_message) - 1)
@@ -57,12 +61,9 @@ with open('dados.json', 'w', encoding='utf-8') as arquivo_json:
                  'avg_amount_spent': cliente['avg_amount_spent'],
                  'e_mail': cliente['e_mail'],
                  'phone': cliente['phone'],
-                 "news": news
+                 'news': news
                  }
 
-        lista.append(dados)
-        
-        print(dados)							
+        lista.append(dados)							
     json.dump(lista, arquivo_json, indent=4, ensure_ascii=False)
-    #print(indice)
-    # print(cliente['first_name'])
+
